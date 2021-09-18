36a23ea9f102098ccbaba4a5b4b52acf2574e023
JPDC98 <3017711380101@ingenieria.usac.edu.gt>
2021-09-18 13:27:42 -0600

    Fase2

---
+++ Codigo_Fases_Proyecto/Fase_2/Fase_2.ino
@@ -0,0 +1,69 @@
+//Compontente a utilidar 
+
+//Pines para controlar la velocidad de las llantas
+const int llanta_1 = 4;
+const int llanta_2 = 5;
+
+//Pines del Ultrasonico
+const int TRIG = 22;
+const int ECO = 24;
+int duracion;
+int distancia;
+
+//Variable de muestreo de las llantas
+const int divisor = 10;
+
+//
+volatile int conteo_0 = 0;
+volatile int conteo_1 = 0;
+
+void setup() {
+  pinMode(llanta_1, OUTPUT);
+  pinMode(llanta_2, OUTPUT);
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECO,INPUT);
+  Serial.begin(9600);
+  attachInterrupt(0,interrupcion_1,RISING);
+  attachInterrupt(1,interrupcion_2,RISING);
+}
+
+  //Serial.print("RPS_1:");
+  //Serial.print(conteo_0*divisor);
+  //Serial.print("  RPS_2:");
+  //Serial.println(conteo_1*divisor);
+
+  
+void loop() {
+  delay(1000/divisor);
+  conteo_0=0;
+  conteo_1=0;
+  digitalWrite(TRIG,HIGH);
+  delay(1);
+  digitalWrite(TRIG,LOW);
+  duracion = pulseIn(ECO,HIGH);
+  distancia = duracion/58.2; 
+  Serial.println(distancia);
+  if(distancia >= 50 and distancia < 60){
+    analogWrite(llanta_1,190);
+    analogWrite(llanta_2,190);
+  }
+  else if(distancia >= 20 and distancia < 50){
+    analogWrite(llanta_1,80);
+    analogWrite(llanta_2,80);
+  }
+  else if(distancia >= 0 and distancia < 20){
+    analogWrite(llanta_1,0);
+    analogWrite(llanta_2,0);
+  }
+  else{
+    analogWrite(llanta_1,255);
+    analogWrite(llanta_2,255);
+  }
+}
+
+void interrupcion_1(){
+  conteo_0++;  
+}
+void interrupcion_2(){
+  conteo_1++;  
+}
